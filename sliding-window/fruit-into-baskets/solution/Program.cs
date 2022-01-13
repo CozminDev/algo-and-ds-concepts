c79f57ba59b86f9d2061bad34804501720dd83df
Cozmin <cozmindeveloper@yahoo.com>
2022-01-13 14:49:33 +0200

    sliding-window fruits-into-basket problem solution

---
+++ sliding-window/fruit-into-baskets/solution/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.TotalFruit(new int[] { 0, 1, 2, 2 }));
+        }
+    }
+
+    class Solution
+    {
+        public int TotalFruit(int[] fruits)
+        {
+            int i = 0;
+            int j = 0;
+            int max = 0;
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            while (j < fruits.Length)
+            {
+                if (!dic.ContainsKey(fruits[j]))
+                    dic.Add(fruits[j], 1);
+                else
+                    dic[fruits[j]]++;
+
+                if (dic.Count > 2)
+                {
+                    dic[fruits[i]]--;
+
+                    if (dic[fruits[i]] == 0)
+                        dic.Remove(fruits[i]);
+
+                    i++;
+                }
+
+                j++;
+                max = Math.Max(max, j - i);
+            }
+
+            return max;
+        }
+    }
+}
