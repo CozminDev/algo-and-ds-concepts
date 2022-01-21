d9a9156f4b461ffc000c0a439310073dda74eb6e
Cozmin <cozmindeveloper@yahoo.com>
2022-01-21 14:21:07 +0200

    merge-intervals meeting-rooms-ii problem and libs

---
+++ merge-intervals/meeting-rooms-ii/solution/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using utility_library;
+
+namespace solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] intervals = new int[][]
+            {
+                new int[] { 0, 30 },
+                new int[] {5, 10},
+                new int[] {15, 20}
+            };
+
+            int[][] intervals2 = new int[][]
+            {
+                new int[] { 7, 10 },
+                new int[] {2, 4},
+            };
+
+            Solution sol = new Solution();
+            sol.MinMeetingRooms(intervals2);
+        }
+
+        public class Solution
+        {
+            public int MinMeetingRooms(int[][] intervals)
+            {
+                intervals = intervals.OrderBy(x => x[0]).ToArray();
+                PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+                foreach (int[] interval in intervals)
+                {
+                    if (pq.Count() == 0 || interval[0] < pq.PeekMin())
+                    {
+                        pq.Enqueue(interval[1], interval[1]);
+                        continue;
+                    }
+
+                    if (interval[0] > pq.PeekMin())
+                    {
+                        pq.DequeueMin();
+                        pq.Enqueue(interval[1], interval[1]);
+                    }
+                        
+                }
+
+                return pq.Count();
+            }
+        }
+    }
+
+
+}
---
+++ utility-library/PriorityQueue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace utility_library
+{
+    public class PriorityQueue<TElement, TPriority>
+    {
+        private SortedList sortedList = new SortedList();
+        private int indexMin = 0;
+        private int indexMax = -1;
+
+        public void Enqueue(TElement element, TPriority priority)
+        {
+            sortedList.Add(priority, element);
+            indexMax++;
+        }
+
+        public TElement DequeueMin()
+        {
+            return (TElement)sortedList.GetByIndex(indexMin++);
+        }
+
+        public TElement DequeueMax()
+        {
+            return (TElement)sortedList.GetByIndex(indexMax--);
+        }
+
+        public TElement PeekMin()
+        {
+            return (TElement)sortedList.GetByIndex(indexMin);
+        }
+
+        public TElement PeekMax()
+        {
+            return (TElement)sortedList.GetByIndex(indexMax);
+        }
+
+        public int Count()
+        {
+            return indexMax - indexMin + 1;
+        }
+    }
+}
