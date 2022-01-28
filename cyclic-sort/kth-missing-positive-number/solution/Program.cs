ee2845479a9b9e99acfd1adcff32632879fd50ec
Cozmin <cozmindeveloper@yahoo.com>
2022-01-28 11:19:37 +0200

    kth-positive-number problem

---
+++ cyclic-sort/kth-missing-positive-number/solution/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            sol.FindKthPositive(new int[] { 2, 3, 4, 7, 11 }, 5);
+        }
+
+
+    }
+
+    public class Solution
+    {
+        //keep 2 indexes i,j
+        //if i == arr[j] increment i and j
+        //if i != arr[j] increment i and decrement k
+        //do this while k >= 0
+        //return i
+        public int FindKthPositive(int[] arr, int k)
+        {
+            int j = 0;
+            int i = 1;
+
+            while(k > 0)
+            {
+                if(j < arr.Length && i == arr[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    k--;
+                }
+            }
+
+            return i - 1;
+        }
+    }
+}
